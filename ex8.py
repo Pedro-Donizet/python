57814c4eec2712c18caa9f755aba99e3c6c5c7de
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-16 12:04:50 -0300

    atividade

--- ex8.py
+++ ex8.py
@@ -1,10 +1,59 @@
-nome = input('digite o nome do vendedor: ')
-quantidade = int(input('quantos carros foram vendidos: '))
-valor = float(input('digite o valor total das vendas: '))
+while True:
+    def menu():
+        escolha = int(input('calculadora\n'
+                            '1- adição\n'
+                            '2- subtração\n'
+                            '3- multiplicação\n'
+                            '4- divisão\n'
+                            '5- sair do programa\n'
+                            'Escolha sua opção:'))
+        return escolha
 
-comissao = 200* quantidade
-acressimo = valor * 2 / 100
+    def adicao(n1, n2):
+        conta = n1 + n2
+        return conta
 
-total = 2500 + 200 * quantidade + valor * 2 / 100
+    def subtracao(n1, n2):
+        conta = n1 - n2
+        return conta
 
-print(f'valor final: {total}')
+    def multiplicacao(n1, n2):
+        conta = n1 * n2
+        return conta
+
+    def divisao(n1, n2):
+        conta = n1 / n2
+        return conta
+
+    escolha = menu()
+    if escolha != 1 and escolha != 2 and escolha != 3 and escolha != 4 and escolha != 5:
+        print('insira uma opçao valda')
+        menu()
+
+    if escolha == 1:
+        numero1 = int(input('insira um numero: '))
+        numero2 = int(input('insira mais um numero: '))
+        adicao(numero1, numero2)
+        print(f'{adicao(numero1, numero2)}')
+
+
+    if escolha == 2:
+        numero1 = int(input('insira um numero: '))
+        numero2 = int(input('insira mais um numero: '))
+        subtracao(numero1, numero2)
+        print(f'{subtracao(numero1, numero2)}')
+    if escolha == 3:
+        numero1 = int(input('insira um numero: '))
+        numero2 = int(input('insira mais um numero: '))
+        multiplicacao(numero1, numero2)
+        print(f'{multiplicacao(numero1, numero2)}')
+
+    if escolha == 4:
+        numero1 = int(input('insira um numero: '))
+        numero2 = int(input('insira mais um numero: '))
+        divisao(numero1, numero2)
+        print(f'{divisao(numero1, numero2)}')
+
+    if escolha == 5:
+        exit()
+
